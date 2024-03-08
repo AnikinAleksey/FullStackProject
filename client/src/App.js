d544ce78ca2d2acdf20f27415499cfc8f49d6ba6
AnikinAV <ankinaleksey2003@mail.ru>
2024-03-08 19:37:27 +0400

    SecondCommit

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <button type="button">Click Me</button>
+        <nav className="nav">
+          <a href="/" className="site-title">
+            <img className="cat1" src="./cat.png" alt="Изображение 1"  />
+            Website Name
+          </a>  
+          <ul>
+            <li className="active">
+              <a className="Page" href="/page1"> Page1</a>
+            </li>
+            <li className="active">
+              <a className= "Page" href="/page2"> Page2</a>
+            </li>
+            <li className="active">
+              <a className= "Page" href="/page3"> Page3</a>
+            </li>
+          </ul>  
+        </nav>
+       
+        
 
     </div>
   );
